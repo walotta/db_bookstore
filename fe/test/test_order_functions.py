8d37bfe8262abe2a1256ddfcb30ebe7041a16b3d
cblmemo <cblmemo@gmail.com>
2023-04-21 07:17:51 +0000

    modify test

--- fe/test/test_order_functions.py
+++ fe/test/test_order_functions.py
@@ -92,7 +92,7 @@ class TestOrderFunctions:
                 tot_price = tot_price + book["price"] * book["count"]
             assert tot_price == self.price_list[i]
 
-    def test_cancel_order(self):
+    def test_cancel_order_ok(self):
         for i in range(self.order_num):
             order_id = self.order_id_list[i]
             status = self.status_list[i]
@@ -100,7 +100,7 @@ class TestOrderFunctions:
                 code = self.buyer.cancel_order(order_id)
                 assert code == 200
 
-    def test_cancel_error_status(self):
+    def test_cancel_order_error(self):
         for i in range(self.order_num):
             order_id = self.order_id_list[i]
             status = self.status_list[i]
