06a3f0aec19b25be9c47c5f1a29b557ef569080f
walotta <walotta@126.com>
2023-04-21 16:13:26 +0800

    basicly finish searcher_interface

--- be/model/db/sub_interface/searcher_interface.py
+++ be/model/db/sub_interface/searcher_interface.py
@@ -4,9 +4,6 @@ from ...template.book_info import BookInfoTemp
 from ..db_client import DBClient
 
 
-# todo: These functions
-
-
 class SearcherInterface:
     def __init__(self, conn: DBClient):
         self.storeCol: Collection = conn.storeCol
@@ -28,7 +25,11 @@ class SearcherInterface:
     def find_book_with_one_dict_n(
         self, dict_name: str, value: Union[int, str], store_id: Optional[str] = None
     ) -> int:
-        pass
+        if store_id is None:
+            query = {dict_name: value}
+        else:
+            query = {"store_id": store_id, dict_name: value}
+        return self.bookInfoCol.count_documents(query)
 
     def find_book_with_one_dict(
         self,
@@ -38,7 +39,7 @@ class SearcherInterface:
         ed: int,
         return_dict: List[str],
         store_id: Optional[str] = None,
-    ) -> List[Dict[str, str]]:
+    ) -> List[Dict[str, Any]]:
         """
         This function could search books with one dict matching.
         Input:
@@ -49,20 +50,33 @@ class SearcherInterface:
             return_dict List[str]
 
         Output:
-            List[List[return_items]]
-            book_number
+            List[Dict[str, Any]]
 
         The st and ed normally satisfy ed-st+1=20, and it will be 1-base
         Nomally we would require 'store_id' and 'book_id' and 'title' in return_dict
         The function returns some books with book[dictName]=value
         This function would not search by book_id
         """
-        pass
+        if store_id is None:
+            query = {dict_name: value}
+        else:
+            query = {"store_id": store_id, dict_name: value}
+        return_filter = {"_id": 0}
+        for i in return_dict:
+            return_filter[i] = 1
+        result = (
+            self.bookInfoCol.find(query, return_filter).skip(st - 1).limit(ed - st + 1)
+        )
+        return [i for i in result]
 
     def find_book_with_content_n(
         self, content_piece: str, store_id: Optional[str] = None
     ) -> int:
-        pass
+        if store_id is None:
+            query = {"content": {"$regex": content_piece}}
+        else:
+            query = {"store_id": store_id, "content": {"$regex": content_piece}}
+        return self.bookInfoCol.count_documents(query)
 
     def find_book_with_content(
         self,
@@ -75,12 +89,29 @@ class SearcherInterface:
         """
         This function returns a book_id which have a part of content_piece
         """
-        pass
+        if store_id is None:
+            query = {"content": {"$regex": content_piece}}
+        else:
+            query = {
+                "store_id": store_id,
+                "content": {"$regex": content_piece},
+            }
+        return_filter = {"_id": 0}
+        for i in return_dict:
+            return_filter[i] = 1
+        result = (
+            self.bookInfoCol.find(query, return_filter).skip(st - 1).limit(ed - st + 1)
+        )
+        return [i for i in result]
 
     def find_book_with_tag_n(
         self, tags: List[str], store_id: Optional[str] = None
     ) -> int:
-        pass
+        if store_id is None:
+            query = {"tags": {"$all": tags}}
+        else:
+            query = {"store_id": store_id, "tags": {"$all": tags}}
+        return self.bookInfoCol.count_documents(query)
 
     def find_book_with_tag(
         self,
@@ -93,4 +124,14 @@ class SearcherInterface:
         """
         This function returns a book_id which have a tag
         """
-        pass
+        if store_id is None:
+            query = {"tags": {"$all": tags}}
+        else:
+            query = {"store_id": store_id, "tags": {"$all": tags}}
+        return_filter = {"_id": 0}
+        for i in return_dict:
+            return_filter[i] = 1
+        result = (
+            self.bookInfoCol.find(query, return_filter).skip(st - 1).limit(ed - st + 1)
+        )
+        return [i for i in result]
