68d17cd4bb339758eb76e8b60bee269b18254c67
walotta <walotta@126.com>
2023-04-21 22:15:47 +0800

    add delete database on each time launch pytest

--- fe/conftest.py
+++ fe/conftest.py
@@ -4,18 +4,25 @@ from urllib.parse import urljoin
 from be import serve
 from fe import conf
 from typing import Optional
+import pymongo
 
 thread: Optional[threading.Thread] = None
 
+def delete_database():
+    client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client.drop_database("bookstore")
+    assert "bookstore" not in client.list_database_names()
 
 # 修改这里启动后端程序，如果不需要可删除这行代码
 def run_backend():
     # rewrite this if rewrite backend
+    delete_database()
     serve.be_run()
 
 
 def pytest_configure(config):
     serve.be_init()
+
     global thread
     print("frontend begin test")
     thread = threading.Thread(target=run_backend)
